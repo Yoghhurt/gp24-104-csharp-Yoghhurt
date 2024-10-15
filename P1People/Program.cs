236c26322839c225d5be4a20dfcab2c5fded0926
JackBillingham <billingham.jacks@gmail.com>
2024-10-15 12:30:10 +0200

    P1_1

---
+++ P1People/Program.cs
@@ -0,0 +1,32 @@
+﻿Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+public class Person
+{
+    public string person;
+
+    public Person()
+    {
+        Console.WriteLine("Person");
+    }
+}
+
+public class Animal
+{
+    public string animal;
+
+    public Animal()
+    {
+        Console.WriteLine("Animal");
+    }
+}
+
+public class Car
+{
+    public string car;
+
+    public Car()
+    {
+        Console.WriteLine("Car");
+    }
+}

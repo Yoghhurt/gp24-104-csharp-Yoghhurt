06a71e962fa941fad0e6acbf58176871def6b90d
JackBillingham <billingham.jacks@gmail.com>
2024-10-16 14:29:50 +0200

    p3

---
+++ P3LevelUpp/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+class Program
+{
+   static void Main()
+   {
+      Player first  = new Player();
+      first.GrantExperience(100);
+   }
+   
+
+   public class Player
+   {
+      public int level;
+      public int experience;
+
+      public void GrantExperience()
+      {
+         experience++;
+         if (experience >= 100)
+         {
+            level++;
+            experience = 0;
+         }
+
+         Console.WriteLine($"Level: {level}, Experience: {experience}");
+
+      }
+
+
+   }
+
+}

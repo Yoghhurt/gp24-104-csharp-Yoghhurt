b33fd27614a7c9d92cb7377917211e7ac01e0321
JackBillingham <billingham.jacks@gmail.com>
2024-10-16 14:07:48 +0200

    p2_2

--- P2_2ItemValue/Program.cs
+++ P2_2ItemValue/Program.cs
@@ -1,7 +1,30 @@
-﻿Item items = new Item();
-items.goldValue[3]; 
-
-public class Item
+﻿
+class Item
 {
-    public int goldValue;
+    public int GoldValue { get; set; }
+
+    public Item(int goldValue)
+    {
+        GoldValue = goldValue;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Item[] items = new Item[3];
+
+        
+        items[0] = new Item(100);
+        items[1] = new Item(200);
+        items[2] = new Item(300);
+
+        
+        for (int i = 0; i < items.Length; i++)
+        {
+            Console.WriteLine($"Item {i + 1} Gold Value: {items[i].GoldValue}");
+        }
+    }
 }

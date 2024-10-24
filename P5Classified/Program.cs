d6cf594a05156d553cdbb945243e9c454fb23bf7
JackBillingham <billingham.jacks@gmail.com>
2024-10-24 14:01:04 +0200

    p5_1

--- P5Classified/Program.cs
+++ P5Classified/Program.cs
@@ -39,3 +39,39 @@ class Sparrow : Animal { }
 class Pigeon : Animal { }
 
 class Hawk : Animal { }
+
+
+class DarthVader
+{
+    public string Name { get; set; } = "Darth Vader";
+    public string Title { get; set; } = "Sith Lord";
+
+    
+
+    public void ShowTitle()
+    {
+        Console.WriteLine($"{Name} is known as the {Title}.");
+    }
+}
+
+
+class Luke : DarthVader
+{
+    
+    
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DarthVader vader = new DarthVader();
+        vader.ShowTitle();
+        
+
+        Luke luke = new Luke();
+        luke.ShowTitle(); 
+        
+    }
+}

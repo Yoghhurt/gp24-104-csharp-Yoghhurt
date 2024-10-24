a00308c0ca8239d7844fb883ddbf8f02bcc5a221
JackBillingham <billingham.jacks@gmail.com>
2024-10-24 13:59:00 +0200

    p5

--- P5Classified/Program.cs
+++ P5Classified/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Item { }
+
+class LivingBeing : Item { }
+
+class Food : Item { }
+
+class Weapon : Item { }
+
+class Animal : LivingBeing { }
+
+class Fruit : Food { }
+
+class Bread : Food { }
+
+class Banana : Fruit { }
+
+class Apple : Fruit { }
+
+class Pear : Fruit { }
+
+class Sword : Weapon { }
+
+class Axe : Weapon { }
+
+class Pencil : Item { }
+
+class Shield : Weapon { }
+
+class Spear : Weapon { }
+
+class Tree : LivingBeing { }
+
+class Gorilla : Animal { }
+
+class Chimpanzee : Animal { }
+
+class Sparrow : Animal { }
+
+class Pigeon : Animal { }
+
+class Hawk : Animal { }
